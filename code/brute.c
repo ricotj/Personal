c36edce535208ddbe21e2bb1ae3004075e043d21
ricotj <ricotj@users.noreply.github.com>
2015-02-05 10:27:27 -0800

    First checkin, created skel makefile and some code

---
+++ code/brute.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  brute.c
+ *
+ *    Description:  Simple brute forcer
+ *
+ *        Version:  1.0
+ *        Created:  02/03/2015 02:39:00 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Trevor J Taylor (TJT), ricotj@gmail.com
+ *        Company:
+ *
+ * =====================================================================================
+ */
+
+
+
+const unsigned int CHAR_OFFSET = 32;
+const unsigned int CHARS = 95;
+
